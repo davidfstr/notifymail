fdb3340a69ea148de6863f902174ff7bf1de591f
David Foster <davidfstr@gmail.com>
2013-07-22 20:28:43 -0700

    Install notifymail as script in addition to as module.

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from distutils.core import setup
 setup(
+    # Identity
     name='notifymail',
     version='1.0',
+    
+    # Contents
     py_modules=['notifymail'],
+    scripts=['notifymail'],
+    
+    # Metadata
     author='David Foster',
     author_email='davidfstr@gmail.com',
     url='https://github.com/davidfstr/notifymail/',
