585ae19eba35db57e969941ea2c340bd3be499d8
David Foster <davidfstr@gmail.com>
2013-07-27 15:04:26 -0700

    Gah. Typos.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def is_older_than(file1, file2):
     return os.path.getmtime(file1) < os.path.getmtime(file2)
 
 # Generate README.rst if missing or out of date
-if not os.path.exists('README.rst') and is_older_than('README.rst', 'README.md'):
+if not os.path.exists('README.rst') or is_older_than('README.rst', 'README.md'):
     os.system('pandoc --from=markdown --to=rst --output=README.rst README.md')
 with open('README.rst') as file:
     long_description = file.read()
